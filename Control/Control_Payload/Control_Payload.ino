5064bc61126b9a63d2b88c0f9f2235994a4748b7
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2024-02-23 14:19:19 -0700

    Finished Control software.
    Payload 1 software is complete, must change 10 milliseconds to 10 seconds.

--- Control/Control_Payload/Control_Payload.ino
+++ Control/Control_Payload/Control_Payload.ino
@@ -7,26 +7,29 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-File myFile;
+File output_file;
 Adafruit_BME680 bme; 
 
+// Defning timer periods (milliseconds)
 const int FIVE_SECONDS = 5000;
 const int TEN_SECONDS  = 10000;
 
+// Runs once on start.
 void setup() {
-  Serial.begin(9600);  // Setting up Serial
-
-  Serial.print("Initializing SD card...");
+  // Setting up SD card. 
   if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
     while (1);
   }
-  Serial.println("initialization done.");
 
-  // open the file. note that only one file can be open at a time,
+  // Open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  myFile = SD.open("data.csv", FILE_WRITE);
+  output_file = SD.open("data.csv", FILE_WRITE);
 
+  // Write the header line:
+  // Must be consistent with the order which we write the data. 
+  output_file.println("temp,hum,pressure,alt,gas_r");
+
+  // Starting the BME 
   if (!bme.begin()) {
     Serial.println("Could not find the BME sensor!");
   }
@@ -39,22 +42,22 @@ void setup() {
   bme.setGasHeater(320, 150);  // 320*C for 150 ms
 }
 
+// Loops forever
 void loop() {
-  if (!bme.performReading()) {
-    Serial.println("Failed to perform reading :(");
-    return;
+  // If we could get readings from BME && could write to file, 
+  // write 1 line to output_file. 
+  if (bme.performReading() && output_file) {
+    output_file.print(bme.temperature);  output_file.print(",");
+    output_file.print(bme.humidity); output_file.print(",");
+    output_file.print(bme.pressure); output_file.print(",");
+    output_file.print(bme.readAltitude(SEALEVELPRESSURE_HPA)); output_file.print(",");
+    output_file.print(bme.gas_resistance);
+    output_file.println();
+    output_file.flush();
+  } else {  // If we could not read data, wirte error to file. 
+    output_file.println("Could not read BME sensor. :(");
   }
 
-  if(myFile) {
-    myFile.print(bme.temperature);  myFile.print(",");
-    myFile.print(bme.humidity); myFile.print(",");
-    myFile.print(bme.pressure); myFile.print(",");
-    myFile.print(bme.readAltitude(SEALEVELPRESSURE_HPA)); myFile.print(",");
-    myFile.print(bme.gas_resistance);
-    myFile.println();
-    myFile.flush();
-  }
-
-  Serial.println();
-  delay(10);
+  // TODO: Wait for 10 seconds
+  delay(10); // <- Milliseconds
 }
